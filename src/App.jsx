436150d5a4876da535a466f33a990c86544d7210
SarwarMir <sarwar.marvelminds@gmail.com>
2025-04-25 12:40:34 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="w-full h-screen grid place-items-center">
+      <div className="w-[70vw] h-[85vh] min-w-[70vw] min-h-[85vh] max-h-[90vh] max-w-[90vw] border border-gray-300 rounded-3xl resize "></div>
+    </div>
+  );
+};
+
+export default App;
